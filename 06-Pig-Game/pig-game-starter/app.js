05b3996459dc15589d7ba6e6430ddf76d96476e7
Regina Pielago <pielago.mrt@gmail.com>
2020-11-27 22:46:55 +0300

    last commit

--- 06-Pig-Game/pig-game-starter/app.js
+++ 06-Pig-Game/pig-game-starter/app.js
@@ -15,112 +15,89 @@ let globalScore, roundScore, currentPlayer, playGame;
 /*********************************************************************/
 
 
-/* ------------------ assigning variables for html ---------------- */
-let globalScorePlayer1 = document.querySelector('#score-0');
-let globalScorePlayer2 = document.querySelector('#score-1');
+/* ------------------ assigning variables for html ----------------- */
+let totalScore = document.querySelectorAll('.player-score');
+let totalScoreP1 = document.querySelector('#score-0');
+let totalScoreP2 = document.querySelector('#score-1');
 
-let roundScorePlayer1 = document.querySelector('#current-0');
-let roundScorePlayer2 = document.querySelector('#current-1');
+let currentScore = document.querySelectorAll('.player-current-score');
+let currentScoreP1 = document.querySelector('#current-0');
+let currentScoreP2 = document.querySelector('#current-1');
 
-let divPlayer1 = document.querySelector('.player-0-panel');
-let divPlayer2 = document.querySelector('.player-1-panel');
+let textP1 = document.querySelector('#name-0');
+let textP2 = document.querySelector('#name-1');
 
-let textPlayer1 = document.querySelector('#name-0');
-let textPlayer2 = document.querySelector('#name-1');
+let divP1 = document.querySelector('.player-0-panel');
+let divP2 = document.querySelector('.player-1-panel');
 
 let dice = document.querySelector('.dice');
 
-let threshold = document.querySelector('#threshold');
+let threshold = document.querySelector('#threshold'); // The input event fires when the value of an <input> element has been changed.
 /*********************************************************************/
 
 
-/* --------------------- initialize game record ------------------- */
-newGame();
+/*-------------------- determining current player ---------------------*/
+
+// changing text content depending on the current player
+function playerTextContent (player1, player2, value) {
+    if (currentPlayer === 0) {
+        player1.textContent = value;
+    } else {
+        player2.textContent = value;
+    }
+}
+
+//changing CSS depending on the current player
+function inactive () {
+    if (currentPlayer === 0) {
+        divP1.classList.add('active'); 
+        divP2.classList.remove('active');
+    } else {
+        divP1.classList.remove('active');
+        divP2.classList.add('active'); 
+    }
+}
+
+// adding CSS to winning player
+function winnerCSS (player1, player2) {
+    if (currentPlayer === 0) {
+        player1.classList.add('winner');
+        player1.classList.remove('active');
+    } else {
+        player2.classList.add('winner');
+        player2.classList.remove('active');
+    }
+}
+
+// next player's turn
+function nextPlayer () {
+    if (currentPlayer === 0) { // if Player 1('0') is active Player, nextPlayer would be Player 2('1')
+        currentPlayer = 1;
+    } else {
+        currentPlayer = 0; // if Player 2('1') is the active Player, nextPlayer would be Player 1('0')
+    }
+    
+    inactive(); // next player should be currently inactive(CSS)
+    // reset current scores for both players before the next player's turn (not doing so would pass the accumulated score of previous player to the next player)
+    roundScore = 0 
+	for (let i of currentScore) {
+        i.textContent = 0;
+    }
+}
 /*********************************************************************/
+
 
 
 /* ----------------------- executing functions -------------------- */
-document.querySelector('.btn-new').addEventListener('click', newGame);
 document.querySelector('.btn-roll').addEventListener('click', rollDice);
 document.querySelector('.btn-hold').addEventListener('click', holdButton);
+document.querySelector('.btn-new').addEventListener('click', reload);
 /*********************************************************************/
-
-
-
-/*********************************************************************/
-/* -------------------------- TAKING TURNS --------------------------*/
-
-function nextPlayer() {
-
-    if (currentPlayer === 0) { // if Player 1('0') is active Player, nextPlayer would be Player 2('1')
-        currentPlayer = 1;
-        roundScore = 0; // removing this would pass the accumulated score to the next player
-    } else {
-        currentPlayer = 0; // if Player 2('1') is the active Player, nextPlayer would be Player 1('0')
-        roundScore = 0; 
-    }
-
-    // reset current score for the next player's turn
-	roundScorePlayer1.textContent = '0';
-	roundScorePlayer2.textContent = '0';
-
-    // toggling 'active' (adding if absent; removing if present)
-	divPlayer1.classList.toggle('active'); 
-	divPlayer2.classList.toggle('active');
-}
-/* ----------------------- End of TAKING TURNS ----------------------*/
-/*********************************************************************/
-
-
-
-/*********************************************************************/
-/* ------------------------- NEW GAME BUTTON ------------------------*/
-
-function newGame () { // clear all records
-
-    globalScore= [0,0]; // global score
-    roundScore = 0; // current score
-    currentPlayer = 0; // Player 1
-    playGame = true; // game will start
-
-    // entered value will be threshold
-    threshold.addEventListener('keypress', function () { threshold.value; });
-
-    // html default value upon refresh
-    threshold.value = threshold.defaultValue;
-
-    // need to set since html shows a non-zero number
-    globalScorePlayer1.textContent = '0';
-    globalScorePlayer2.textContent = '0';
-    roundScorePlayer1.textContent = '0';
-    
-    // to remove 'winner' text for the new game
-    textPlayer1.textContent = 'Player 1';
-    textPlayer2.textContent = 'Player 2';
-    
-    // no dice displayed initially
-    dice.style.display = 'none';
-    
-    // reset related line in function holdButton
-    divPlayer1.classList.remove('active');
-    divPlayer2.classList.remove('active');
-    divPlayer1.classList.remove('winner');
-    divPlayer2.classList.remove('winner');
-    
-    // highlight on Player 1 by default
-    divPlayer1.classList.add('active');
-}
-/* --------------------- End of NEW GAME BUTTON ---------------------*/
-/*********************************************************************/
-
 
 
 /*********************************************************************/
 /* ------------------------ ROLL DICE BUTTON ------------------------*/
-
 function rollDice () {
-    
-    let currentPlayerRoundScore = document.querySelector(`#current-${currentPlayer}`);
         
     if(playGame) { 
         let diceNum = Math.floor(Math.random() * 6) + 1; // +1 so 0 will not be generated
@@ -132,7 +109,7 @@ function rollDice () {
             dice.style.display = 'none'; // dice-1 will not be displayed
         } else {
             roundScore += diceNum; // score accumulation per dice rolling
-            currentPlayerRoundScore.textContent = roundScore; // shows accumulated current score
+            playerTextContent(currentScoreP1, currentScoreP2, roundScore); // displays accumulated current score
         }
     }
 };
@@ -143,34 +120,62 @@ function rollDice () {
 
 /*********************************************************************/
 /* --------------------------- HOLD BUTTON --------------------------*/
-
 function holdButton () {
-
-    let currentPlayerGlobalScore = document.querySelector(`#score-${currentPlayer}`);
-    let currentPlayerRoundScore = document.querySelector(`#current-${currentPlayer}`);
-    let currentPlayerText = document.querySelector(`#name-${currentPlayer}`);
-    let currentPlayerDiv = document.querySelector(`.player-${currentPlayer}-panel`);
      
     if(playGame) {
         globalScore[currentPlayer] += roundScore; // score accumulation per hold 
-        currentPlayerGlobalScore.textContent = globalScore[currentPlayer]; // shows accumulated global score
+        playerTextContent(totalScoreP1, totalScoreP2,globalScore[currentPlayer]); // shows accumulated global score
 
 	    if(globalScore[currentPlayer] >= threshold.value) { // threshold score
-                currentPlayerText.textContent = 'Winner!'; // Player's name changed to 'winner'
-                currentPlayerRoundScore.textContent = 0; // current score displays 0
+                playerTextContent(textP1, textP2, 'Winner!'); // Player's name changed to 'winner'
+                playerTextContent(currentScoreP1, currentScoreP2, 0); // current score displays 0
+                winnerCSS(divP1, divP2); // apply CSS
                 dice.style.display = 'none';
-                
-                // game will stop
-                currentPlayerDiv.classList.add('winner');
-                currentPlayerDiv.classList.remove('active');
-                playGame = false;
+                playGame = false; // game will stop
 	    } else {
                 nextPlayer(); // continue the game; switch to another player
 	    }
     }
+
 };
 /* ----------------------- End of HOLD BUTTON -----------------------*/
 /*********************************************************************/
 
 
 
+/*********************************************************************/
+/* ------------------------------- RESET ----------------------------*/
+function reload () { 
+    location.reload(); 
+    return false; // reloads the page using the browser’s cached data (if true, reloads the page from the server and does not store the data cached by the browser)
+};
+/* --------------------------- End of RESET -------------------------*/
+/*********************************************************************/
+
+
+/*********************************************************************/
+/* ------------------------- NEW GAME BUTTON ------------------------*/
+function newGame () { // clear all records
+
+    globalScore = [0,0]; // total score
+    roundScore = 0; // current score (if not set, first value will be NaN)
+    currentPlayer = 0; // Player 1
+    playGame = true; // game will start
+
+    // need to set since html shows a non-zero number
+    for (let i of totalScore) {
+        i.textContent = 0;
+    };
+
+    for (let i of currentScore) {
+        i.textContent = 0;
+    };
+   
+    // no dice displayed initially
+    dice.style.display = 'none';   
+};
+
+newGame(); // self-executing on page load
+
+/* --------------------- End of NEW GAME BUTTON ---------------------*/
+/*********************************************************************/
