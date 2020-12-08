890de7a7382a474afbd6a1f7306178bdab746996
Regina Pielago <pielago.mrt@gmail.com>
2020-12-08 13:26:26 +0300

    WIP

--- 07-Banking-App/script.js
+++ 07-Banking-App/script.js
@@ -2,161 +2,37 @@
 let accountName = document.querySelector('#display-name');
 let currentBalance = document.querySelector("#bal-end-val");
 let customerAccount = document.querySelector('#customer-account');
+let login = document.querySelector("#login");
+let loginName = document.querySelector('#customer-name-login');
+let loginPassword = document.querySelector('#password-login');
+let loginLink = document.querySelector('#reg-to-login');
+let loginBtn = document.querySelector("#login-btn");
 let logoutBtn = document.querySelector('#logout');
 let newUser = document.querySelector('#customer-name');
 let newUserPassword = document.querySelector('#password');
 let newUserConfirmPassword = document.querySelector('#confirm-password');
 let registration = document.querySelector("#registration");
 let registrationBtn = document.querySelector("#reg-btn");
+let registrationLink = document.querySelector("#reg-link");
 let submitBtn = document.querySelector("#submit-transaction");
 let transactionAmount = document.querySelector("#transaction-amount");
+let transactionTransferAlert = document.querySelector("#transaction-alert");
 let transactions = document.querySelector("#transactions")
-let transactionType = document.querySelector("#transaction-type") 
+let transactionType = document.querySelector("#transaction-type")
+let transferInputText = document.querySelector('#transfer-text'); 
 let transactionTable = document.querySelector("#transaction-table");
 /*******************************************************************/
 
 /************************* EVENT LISTENERS *************************/
-registrationBtn.addEventListener("click", toggleCustomerAccount);
-registrationBtn.addEventListener("click", displayCustomerAccount);
-registrationBtn.addEventListener("click", () => { hideToggle(registration); });
+registrationLink.addEventListener("click", registerLink);
 registrationBtn.addEventListener("click", registerHere);
-submitBtn.addEventListener("click", () => { checkTransaction(); });
-submitBtn.addEventListener("click", submitHere);
+submitBtn.addEventListener("click",transactionLogin);
+loginBtn.addEventListener("click", () => { accountLogin(loginName.value) });
+loginLink.addEventListener('click', logoutFunction);
 logoutBtn.addEventListener('click', logoutFunction);
+transactionType.addEventListener("click", toggleTransferText);
 /********************************************************************/
 
-
-/********************************************************************/
-/***************************** DISPLAY ******************************/
-
-/************************** REUSABLE FUNCTIONS **********************/
-// Clears element html
-function clearValues(elementID) { 
-    document.getElementById(elementID).value = "";
-}
-
-// for hiding items
-function hideToggle(item) {
-    if (item.style.display === "none") {
-        item.style.display = "block";
-    } else {
-        item.style.display = "none";
-    }
-}
-
-// added to transaction history as they are entered
-function addTransaction(table, account) { 
-    let newRow = table.insertRow(2);
-    let descriptionCell = newRow.insertCell(0);
-        descriptionCell.innerHTML = account.allTransactions[account.allTransactions.length - 1].type;
-        descriptionCell.style.textAlign = 'left';
-    let amountCell = newRow.insertCell(1);
-        amountCell.innerHTML = account.allTransactions[account.allTransactions.length - 1].amount;
-    let balanceCell = newRow.insertCell(2);
-        balanceCell.innerHTML = account.balance;
-
-    if (descriptionCell.innerHTML === 'Deposit') {
-        amountCell.style.color = '#008140';
-    } else if (descriptionCell.innerHTML === 'Withdraw') {
-        amountCell.style.color = 'red';
-    }
-}
-/********************************************************************/
-
-let account = {
-    balance: 0,
-    allTransactions: [],
-    deposit: function (amount) {
-        this.balance += amount;
-        this.allTransactions.push({
-            type: "Deposit",
-            amount: amount,
-        })
-    },
-    withdraw: function (amount) {
-        this.balance -= amount;
-        this.allTransactions.push({
-            type: "Withdraw",
-            amount: amount,
-        })
-    },   
-}
-
-
-// executes depending on transaction type selected
-function checkTransaction() {
-    if (transactionType.value === "Deposit") {
-        depositFunction();
-        transactionAmount.value = "";
-    } else if (transactionType.value === "Withdraw") {
-        withdrawFunction();
-        transactionAmount.value = "";
-    }
-}
-
-
-function depositFunction() {
-    let depositValue = Number.parseFloat(transactionAmount.value);
-    if (isNaN(depositValue)) {
-        alert("Please enter an amount.");
-        } else if (depositValue <= 0) {
-            alert("Please enter a positive number.")
-        } else {
-            account.deposit(depositValue);
-            displayValues();
-            addTransaction(transactionTable, account);
-        } 
-}
-
-
-function withdrawFunction() {
-    let withdrawnValue = Number.parseFloat(transactionAmount.value);
-
-    if (isNaN(withdrawnValue)) {
-        alert("Please enter an amount.");
-        } else if (withdrawnValue <= 0) {
-            alert("Please enter a positive number.");
-        } else {
-            if (account.balance >= withdrawnValue) {
-                account.withdraw(withdrawnValue);
-                displayValues();
-                addTransaction(transactionTable, account);
-            } else {
-                alert("Not enough funds for the transaction.")
-            }
-    }  
-}
-
-
-
-// to hide customer info
-function toggleCustomerAccount() {
-    hideToggle(customerAccount);
-}
-
-toggleCustomerAccount(); // to hide customer info on load
-
-
-// to display updated values
-function displayValues () { 
-    accountName.textContent = newUser.value.toUpperCase();
-    currentBalance.textContent = account.balance;
-}
-
-// for execution on click (registrationBtn)
-function displayCustomerAccount () {
-    clearValues("registration");
-    displayValues(); 
-}
-
-// for execution on click (logoutBtn)
-function logoutFunction () {
-    customerAccount.style.display ='none';
-    registration.style.display = 'block';
-    newUser.value = '';
-    newUserPassword.value = '';
-    newUserConfirmPassword.value = '';
-}
 
 /********************************************************************/
 /************************* REQUIRED FUNCTIONS ***********************/
@@ -171,45 +47,92 @@ let User = function (name, password) {
         this.amount += amount;
     }
     this.withdrawThis = function (amount) {
-        this.amount -= amount;
+        if (this.amount >= amount) {
+            this.amount -= amount;
+        } else {
+            console.log('Insufficient funds!')
+            alert('Funds are not enough to proceed with the transaction.')
+        }
     };
-};
+}
 
 /*------------------------create_user (user)------------------------*/
 function createUser (name, password) {
     let user = accountHolders.some(u => u.name == name);
     if (user === true) {
         console.log('%c User already exists! ', 'background: red; color: yellow')
+        alert('Bank Account is already registered.')
+        customerAccount.style.display = "none";
+        registration.style.display ="block";
+        newUser.value ="";
+        newUserPassword.value = "";
+        newUserConfirmPassword.value = "";
     } else {
         let createNewUser = new User(name, password);
         accountHolders.push(createNewUser);
     }
 };
 
+// registration
 function registerHere () {
     let letters = /^[A-Za-z]+$/;
-    if(newUser.value.match(letters)) {
+    if(newUser.value.match(letters) && newUserPassword.value === newUserConfirmPassword.value && newUserPassword.value.length > 0) {
         createUser(newUser.value, newUserPassword.value);
+        login.style.display = "block";
+        registration.style.display = "none";
+        alert('REGISTRATION SUCCESSFUL')    
     } else {
-        alert('Please enter alphabet characters only');
+        alert('Passwords are not matching.');
         console.log('%c WRONG ARGUMENTS! ', 'color: red; font-weight: bold');
         customerAccount.style.display = 'none';
         registration.style.display ="block";
+        newUser.value ="";
+        newUserPassword.value = "";
+        newUserConfirmPassword.value = "";
     }
- }
+}
 
-// validation
-function userExists(name) {
+function registerLink () {
+    if (registration.style.display = "none") {
+        registration.style.display = "block";
+        customerAccount.style.display = "none";
+        login.style.display = "none";
+
+    } else {
+        registration.style.display ="none";
+    }
+}
+
+function accountLogin (name) {
     let user = accountHolders.some(u => u.name == name);
     if (user === true) {
-        console.log('%c User already exists! ', 'background: red; color: yellow')
+        customerAccount.style.display = "block";
+        login.style.display = "none";
+        accountName.textContent = loginName.value.toUpperCase();
+        currentBalance.textContent = numberWithCommas(getBalance(loginName.value)); 
     } else {
         console.log('%c User does not exist! ', 'background: yellow; color: red')
+        alert('Bank account not registered.');
+        loginName.value = '';
+        loginPassword.value = '';
     }
 };
 
 
-/*------------deposit (user, amount) / withdraw (user, amount)-----------*/
+// for execution on click (logout)
+function logoutFunction () {
+    customerAccount.style.display ='none';
+    registration.style.display = 'none';
+    login.style.display = 'block';
+    newUser.value = '';
+    newUserPassword.value = '';
+    newUserConfirmPassword.value = '';
+    loginName.value = '';
+    loginPassword.value = '';
+}
+
+
+/*------------deposit (user, amount) / withdraw (user, amount) / send (from_user, to_user, amount)-----------*/
 function deposit(name, amount) {
     let user = accountHolders.find(u => u.name === name);
     user.depositThis(amount);
@@ -222,31 +145,56 @@ function withdraw(name, amount) {
     return user.amount
 }
 
-function submitHere () {
-     if (transactionType.value === "Deposit") {
-        deposit(newUser.value, account.allTransactions[account.allTransactions.length - 1].amount);
-    } else if (transactionType.value === "Withdraw") {
-        withdraw(newUser.value, account.allTransactions[account.allTransactions.length - 1].amount);
-    } 
-}
-
-/*------------------send (from_user, to_user, amount)--------------------*/
 function send (from_name, to_name, amount) {
     let sender = accountHolders.find(u => u.name === from_name);
     let senderExists = accountHolders.some(u => u.name === from_name);
     let receiver = accountHolders.find(u => u.name === to_name);
     let receiverExists = accountHolders.some(u => u.name === to_name);
 
-
-    if (senderExists === true && receiverExists === true) {
+    if (sender === receiver) {
+        alert('Cannot transfer to own account');
+    } else if (receiverExists === true) {
         sender.withdrawThis(amount);
         receiver.depositThis(amount);
-    } else if (senderExists === false && receiverExists === true) { 
-        console.log('%c SENDER does not exist!', 'color: red')
-    } else if (receiverExists === false && senderExists === true) {
-        console.log('%c RECEIVER does not exist!', 'color: red')
-    } 
+        alert('You have successfully transferred funds to another account.')
+    } else if (receiverExists === false) {
+        console.log('%c RECEIVER does not exist!', 'background: yellow; color: red')
+        alert('RECEIVER does not exist');
+    } else if (senderExists === false) {
+        console.log('%c SENDER does not exist!', 'background: yellow; color: red')
+        alert('SENDER does not exist');
+    }
 }
+
+
+function transactionLogin () {
+    if (transactionType.value === "Deposit") {
+        deposit(loginName.value, Number.parseFloat(transactionAmount.value));
+    } else if (transactionType.value === "Withdraw") {
+        withdraw(loginName.value, Number.parseFloat(transactionAmount.value));
+    } else if (transactionType.value = "Transfer") {
+        send(loginName.value, transferInputText.value, Number.parseFloat(transactionAmount.value));
+        transferInputText.style.display = "none"
+    }
+    transactionType.value = "Deposit";
+    transactionAmount.value = "";
+    transferInputText.value = "";
+    currentBalance.textContent = numberWithCommas(getBalance(loginName.value));
+}
+
+
+function toggleTransferText() {
+    // Checks transaction type, shows input box when 'transfer' is selected
+    if (transactionType.value === "Transfer") {
+        transferInputText.style.display = "block";
+    } else if (transactionType.value === "Withdraw") {
+        transferInputText.style.display = "none";
+    } else if (transactionType.value === "Deposit") {
+        transferInputText.style.display = "none";
+    }
+}
+
+
 
 /*-------------------------get_balance (user)-----------------------------*/
 function getBalance (name) {
@@ -254,26 +202,8 @@ function getBalance (name) {
     return user.amount;
 }
 
+function numberWithCommas(amount) {
+    return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 console.log(accountHolders);
-
-/*********************** VALIDATION ****************************/
-
-
-// function enterPassword () {
-//     if (newUserPassword.value.length <=0) {
-//         alert('Please enter your password.')
-//         console.log('wrong_arguments')
-//         customerAccount.style.display = 'none';
-//         registration.style.display ="block";
-//     } else if (newUserConfirmPassword.value !== newUserPassword.value) {
-//         alert('Passwords are not matching.')
-//         console.log('wrong_arguments')
-//         customerAccount.style.display = 'none';
-//         registration.style.display ="block";
-//     } else {
-//         customerAccount.style.display = 'block';
-//         registration.style.display ="none";
-//     }
-// }
-
