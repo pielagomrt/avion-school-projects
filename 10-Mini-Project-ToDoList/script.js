d031a9c75d8017d6d3c9bb7cd0c173402069070b
Regina Pielago <pielago.mrt@gmail.com>
2020-12-22 06:19:44 +0300

    WIP

--- 10-Mini-Project-ToDoList/script.js
+++ 10-Mini-Project-ToDoList/script.js
@@ -1,90 +1,55 @@
-// accessing html
-let dateToday = document.querySelector("#date");
-let toDoContainer = document.querySelector('#toDoContainer');
-let newToDo = document.querySelector('#toDoInput');
-let addButtonId = document.getElementById("addButton");
+/*-------------------accessing html-------------------*/
+let dateToday = document.querySelector('#date');
+let input = document.getElementById("input");
+let list = document.querySelector('ul');
+let addBtn = document.querySelector('#addBtn');
 
-// event listeners
-addButtonId.addEventListener("click", addToDo);
+/*-------------------event listeners-------------------*/
+list.addEventListener('click', crossedOut);
+addBtn.addEventListener('click', addItem);
 
+/*-------------------functions-------------------*/
 
-const toDoItems = [];
+// current date
+(function currentDate () {
+  let today = new Date();
+  let date = `${today.getMonth()+1} / ${today.getDate()} / ${today.getFullYear()}`;
+  dateToday.textContent = date;
+})();
 
-let today = new Date();
-let date = `${today.getMonth()+1} / ${today.getDate()} / ${today.getFullYear()}`;
-dateToday.innerHTML = date;
+// creating a new item
+function addItem() {
+  let li = document.createElement("li");
+  let span = document.createElement("SPAN");
+  let task = document.createTextNode(input.value);
+  let x = document.createTextNode("\u00D7");
 
-function ToDo(description) {
-  this.description = description;
-  this.complete = false;
-  this.completeToDo = function () {
-    this.complete = true;
+  li.appendChild(task);
+  li.appendChild(span);
+  span.appendChild(x);
+
+  // appending elements
+  if (input.value !== '') {
+    list.appendChild(li);
+  } else {
+    alert("Please enter a task.");
   }
+  input.value = "";
+ 
+  // close button
+  span.className = "remove";
+  let removeBtn = document.querySelectorAll(".remove");
+  
+  for (let items of removeBtn) {
+    items.addEventListener('click', () => {
+      let itemDiv = items.parentElement;
+      itemDiv.style.display = 'none';
+  })};
 }
 
-function addToDo() {
-  const anyValue = new ToDo(newToDo.value);
-  toDoItems.push(anyValue);
-  newToDo.value = "";
-  displayToDos();
-}
-
-function buildToDo(todo, index) {
-  const toDoShell = document.createElement("div");
-  toDoShell.className = "toDoShell";
-  const toDoText = document.createElement("span");
-  toDoText.innerHTML = todo.description;
-
-  const deleteBtn = document.createElement("input");
-  deleteBtn.id = index;
-  deleteBtn.className = "completeDeleteBtn"; // for css
-  deleteBtn.type = "button";
-  deleteBtn.value = 'X';
-  deleteBtn.addEventListener("click", completeToDo);
-
-  let deleteBtnStyles = `
-    background: transparent;
-    font-weight: bold;
-    color: navy;
-    outline: none;
-    border-radius: 50%;
-    border: 2px solid navy;
-    padding: 3px 6px 2px 6px;
-    margin-right: 20px;
-    cursor: pointer`
-
-  deleteBtn.style = deleteBtnStyles;
-
-  if (todo.complete === true) {
-    toDoText.className = "completeText";
-    toDoShell.classList.add('completed');
-    deleteBtn.classList.add('completeBtn');
+// crossing out an item on the list
+function crossedOut (e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('done');
   }
-
-  toDoShell.appendChild(toDoText);
-  toDoShell.appendChild(deleteBtn);
-  return toDoShell;
-}
-
-function buildToDos(toDos) {
-  const toDoArray = toDos.map(buildToDo);
-  return toDoArray;
-}
-
-function displayToDos() {
-  toDoContainer.innerHTML = "";
-  const buildToDosItems = buildToDos(toDoItems);
-
-  buildToDosItems.forEach(function (element) {
-    toDoContainer.appendChild(element);
-  });
-}
-
-
-function completeToDo(event) {
-  const index = event.target.id;
-  toDoItems[index].completeToDo();
-  displayToDos();
-}
-
-displayToDos();
+};
